690373c4bc33f66d98e5e1af18b1833c31d2f5a1
Çağrı Tümer <cagritumer0@gmail.com>
2022-04-04 21:40:40 +0300

    Add project files.

---
+++ MailSender/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using MailSender.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MailSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //we will use Index for sending mail. whenever we run the project your mail will be delivered
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+
+            //------------------------------------------------------------------------->
+            // gmail smtp settings
+            //SMTP Host Name: smtp.gmail.com
+            //SMTP user name: gmail mail address //-- example: mail@gmail.com 
+            //SMTP Password: Your Gmail password
+            //SMTP Port: 587 / 465
+            //TLS / SSL: yes
+
+            //Yahoo Mail SMTP Host Address : smtp.mail.yahoo.com
+            //Yahoo Mail SMTP user name : your yahoo mail address xyz@yahoo.com
+            //Yahoo Mail SMTP password : your yahoo mail password
+            //Yahoo Mail SMTP port:465  or 587
+            //Yahoo Mail SMTP TLS / SSL yes
+            //------------------------------------------------------------------------->
+
+            //---->>> 
+            //smtp settings....
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("xyz@gmail.com|| YOUR MAİL ADDRESS", " YOUR PASSWORD");
+            smtp.EnableSsl = true;
+            smtp.Timeout = 10000; // means 10 sec. 1000.000 => 100 sec. try for 100 sec
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            //--->>>
+            //mail settings
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("your mail address", "Your Name ");
+            mail.To.Add("abc@gmail.com ; xyzt@asd.com ; ||| Addresses to which you want to send mail");
+            //dont forget ---" "----  and ----- ; ----- between the addresses 
+            mail.CC.Add("abc@gmail.com ; xyzt@asd.com ; ||| Addresses to which you want to send mail ");//CC => carbon copy
+            mail.Bcc.Add("abc@gmail.com ; xyzt@asd.com ; ||| Addresses to which you want to send mail ");//BCC=> black carbon copy
+            mail.Subject = "SUBJECT";
+            mail.Body = "<h1> WHATEVER YOU WANT TO SAY </h1>";
+            mail.IsBodyHtml = true;
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;//for turkish you can find you language utf online
+            mail.BodyEncoding = System.Text.Encoding.UTF8;////for turkish you can find you language utf online
+            mail.Attachments.Add(new Attachment("E://Downloads/imgs/"));//if you want to add attachments you can use this
+            //sending
+            smtp.Send(mail);
+            mail.Dispose();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
